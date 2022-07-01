1852b4671f16f0980a2069c56bd244da65ca238b
coderkaushal <kaushal120695@gmail.com>
2022-07-01 23:06:26 +0530

    added upto color change

---
+++ Browser_projects/Jira/script.js
@@ -0,0 +1,82 @@
+const allColors = document.querySelectorAll(".color");
+const main = document.querySelector(".main");
+const addBtn = document.querySelector(".add");
+const colors = ["pink", "blue", "green", "black"];
+
+// for (let i = 0; i < allColors.length; i++) {
+//     allColors[i].addEventListener("click", changeColor)
+// }
+// // e-> is the object that will be given by browser
+// function changeColor(e) {
+//     // e.currentarget -> refers the element on which event has 
+//     // occurred
+//     let elem = e.currentTarget;
+//     // all the classes present on the element 
+//     let allclasses = elem.classList;
+//     let color = allclasses[1];
+//     console.log(color);
+//     main.style.backgroundColor = color;
+//     // console.log(elem, " ", allclasses);
+// }
+console.log("uuid", uuidv4());
+
+// creation of tickets
+
+// 1. when + is clicked then a ticket will created
+addBtn.addEventListener("click", function () {
+    // create a ticket 
+    createTicket();
+})
+function createTicket() {
+    // 2. main -> ticket add
+    let id = uuidv4();
+    let ticket = document.createElement("div");
+    ticket.setAttribute("class", "ticket");
+    ticket.innerHTML = `
+    <div class="ticket_header black"></div>
+    <div class="ticket_content">
+        <div class="ticket_id ">
+            #${id}
+        </div>
+        <textarea name=""></textarea>
+    </div>
+`;
+    main.appendChild(ticket);
+    // ticket should have some functionalites
+
+    // 3. change color
+    // header pe click ho -> change the color
+    let header = ticket.querySelector(".ticket_header");
+    header.addEventListener("click", changeColor)
+    // 4. lock and unlock,color filtering 
+    // diificult
+}
+function changeColor(e) {
+    // e.currentTarget give the element on which event has happened
+    let header = e.currentTarget;
+    //    i have html element and classes can be used to identify that
+    //  element and set another class to modify it's behaviour 
+    // list of all the classes on an element 
+    let classes = header.classList;
+    let cColor = classes[1];
+    // alert(cColor);
+    // check idx -> next element uspe put 
+    let cIdx = 0;
+    for (let i = 0; i < colors.length; i++) {
+        if (cColor == colors[i]) {
+            cIdx = i;
+            break;
+        }
+    }
+    let nextIdx = (cIdx + 1) % colors.length;
+    let nextColor = colors[nextIdx];
+    // classes[1]=nextColor;
+    
+    classes.remove(cColor);
+    classes.add(nextColor);
+
+}
+
+
+
+
---
+++ Browser_projects/Jira/todo.md
@@ -0,0 +1,20 @@
+tickets -> header -> 
+color -> oscillatin
+g id -> unique -> library
+ text -> editable or not one some conditions (Lock and unlock)
+  filtering 
+  delete
+
+create , local storage
+
+<!-- to code anything dyanamic -> static version-->
+<!-- html -> tags -->
+<!-- css-->
+
+flex -> height , width : min-height ,min-width
+<!--when you want that element should have minimum that height and if content inside it increases then it should automatically increase it's height-->
+
+if there are multiple files in an html file then -> all the code are combined and it ios treated as one
+
+file-wrap -> if all the elements width is more than the given width of parents
+then wrap will put the extra element in the next line
